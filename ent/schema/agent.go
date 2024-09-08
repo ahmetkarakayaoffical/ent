3b6c8244f12f4cdddfe5eb3cefd5719bdb2f9da5
Miguel Cabrerizo <doncicuto@gmail.com>
2024-09-08 09:40:39 +0200

    initial commit

---
+++ ent/schema/agent.go
@@ -0,0 +1,43 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Agent holds the schema definition for the Agent entity.
+type Agent struct {
+	ent.Schema
+}
+
+// Fields of the Agent.
+func (Agent) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("id").NotEmpty().Unique().StorageKey("oid"),
+		field.String("os").NotEmpty(),
+		field.String("hostname").NotEmpty(),
+		field.String("version").NotEmpty(),
+		field.String("ip").Default(""),
+		field.Time("first_contact"),
+		field.Time("last_contact"),
+		field.Bool("enabled").Default(true),
+	}
+}
+
+// Edges of the Agent.
+func (Agent) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("computer", Computer.Type).Unique().Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("operatingsystem", OperatingSystem.Type).Unique().Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("systemupdate", SystemUpdate.Type).Unique().Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("antivirus", Antivirus.Type).Unique().Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("logicaldisks", LogicalDisk.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("apps", App.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("monitors", Monitor.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("shares", Share.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("printers", Printer.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+		edge.To("networkadapters", NetworkAdapter.Type).Annotations(entsql.Annotation{OnDelete: entsql.Cascade}),
+	}
+}
