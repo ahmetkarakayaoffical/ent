55161d1c3bc0c780fb9376be2ba4ce00b2e09cde
Miguel Cabrerizo <doncicuto@gmail.com>
2025-03-17 11:27:07 +0100

    feat: add new tasks types for group management

--- task/task.go
+++ task/task.go
@@ -169,6 +169,10 @@ const (
 	TypeRemoveRegistryKeyValue        Type = "remove_registry_key_value"
 	TypeAddLocalUser                  Type = "add_local_user"
 	TypeRemoveLocalUser               Type = "remove_local_user"
+	TypeAddLocalGroup                 Type = "add_local_group"
+	TypeRemoveLocalGroup              Type = "remove_local_group"
+	TypeAddUsersToLocalGroup          Type = "add_users_to_local_group"
+	TypeRemoveUsersFromLocalGroup     Type = "remove_users_from_local_group"
 )
 
 func (_type Type) String() string {
@@ -178,7 +182,7 @@ func (_type Type) String() string {
 // TypeValidator is a validator for the "type" field enum values. It is called by the builders before save.
 func TypeValidator(_type Type) error {
 	switch _type {
-	case TypeWingetInstall, TypeWingetUpdate, TypeWingetDelete, TypeAddRegistryKey, TypeUpdateRegistryKeyDefaultValue, TypeAddRegistryKeyValue, TypeRemoveRegistryKey, TypeRemoveRegistryKeyValue, TypeAddLocalUser, TypeRemoveLocalUser:
+	case TypeWingetInstall, TypeWingetUpdate, TypeWingetDelete, TypeAddRegistryKey, TypeUpdateRegistryKeyDefaultValue, TypeAddRegistryKeyValue, TypeRemoveRegistryKey, TypeRemoveRegistryKeyValue, TypeAddLocalUser, TypeRemoveLocalUser, TypeAddLocalGroup, TypeRemoveLocalGroup, TypeAddUsersToLocalGroup, TypeRemoveUsersFromLocalGroup:
 		return nil
 	default:
 		return fmt.Errorf("task: invalid enum value for type field: %q", _type)
